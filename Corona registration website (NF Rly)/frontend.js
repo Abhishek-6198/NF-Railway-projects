9fc0b2cc33e6fe1de5ce066ee94ff9b767c259c4
Abhishek <abhishek.roychoudhury69@gmail.com>
2021-06-16 18:10:57 +0530

    updated

--- Corona registration website (NF Rly)/frontend.js
+++ Corona registration website (NF Rly)/frontend.js
@@ -1,24 +1,27 @@
 function toggle() {
    var x = document.getElementById("myDIV");
+   var z = document.getElementById("disclaimer_points");
    /*var y = document.getElementById("patient_status");*/
    var y = document.getElementsByClassName("registration_content");
    if (x.style.display === "none") {
      x.style.display = "block";
      y[0].style.top="30em";
+     z.innerHTML = "Disclaimer"
    } else {
      x.style.display = "none";
      y[0].style.top="10em";
+     z.innerHTML = "Disclaimer (Click to read)"
    }
  }
 
  function toggle_contact(){
    var x = document.getElementById("contact_info");
-   if (x.style.display === "none") {
-     x.style.display = "block";
-   } else {
-     x.style.display = "none";
+   document.getElementById("contact").onclick = function(){
+     x.show();
    }
-
+   document.getElementById("hide").onclick = function(){
+     x.close();
+   }
  }
 
  function openNav() {
