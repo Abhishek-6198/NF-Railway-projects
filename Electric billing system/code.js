319717bf8d2fff2e6b4ebe60039491b7f1d93da6
Abhishek Roychoudhury <85999287+Abhishek-6198@users.noreply.github.com>
2021-09-23 11:40:10 +0530

    Add files via upload

---
+++ Electric billing system/code.js
@@ -0,0 +1,13 @@
+document.getElementById("register").addEventListener("mouseover",function(){
+    document.getElementById("r").style.fontWeight="500"
+    setTimeout(function(){
+        document.getElementById("r").style.fontWeight="200";
+    },500)
+})
+
+document.getElementById("occupancy").addEventListener("mouseover",function(){
+    document.getElementById("occ").style.fontWeight="500"
+    setTimeout(function(){
+        document.getElementById("occ").style.fontWeight="200";
+    },500)
+})
